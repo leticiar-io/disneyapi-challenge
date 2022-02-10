8630b193109ae8d07261cc41ad7a16f6346f1dee
leticia-rodriguesf <leticia.rodrigues20019@gmail.com>
2022-02-10 10:22:47 -0300

    :tada: Finished Project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import SignInPage from '../layout/SignIn'
+import SignInPage from '../pages/signIn/index'
 
 const Index = () => {
   return (
--- src/pages/signIn/index.js
+++ src/pages/signIn/index.js
@@ -1,6 +1,6 @@
 import SignIn from "../../layout/signIn";
 
-export function SignInPage(){
+export default function SignInPage(){
     return(
         <SignIn />
     )
